293831f146d7f063b1f7227ca350f47666259747
Khaled Taha <khaledtaha105@gmail.com>
2023-08-21 08:14:54 +0300

    Create Employee Model&View

---
+++ SkyLine/SkyLine/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyLine.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
